d7a1cd870c5dd3b839e53b4c11a5375f428177e6
Derik <derik@oagro.com.br>
2020-10-28 18:58:16 -0300

    Creating MyApp class with initial layout

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-     backgroundColor: Colors.lightBlue,
-      body: Container()
-    )
-  ));
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            backgroundColor: Colors.teal,
+            body: SafeArea(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundImage: AssetImage('images/avatar2.jpg'),
+                    radius: 50.0,
+                  ),
+                  Text(
+                    'Derik Gonçalves',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Pacifico',
+                      fontSize: 40.0
+                    ),
+                  ),
+                  Text(
+                    'DEVELOPER',
+                    style: TextStyle(
+                        color: Colors.teal.shade100,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                        letterSpacing: 2.5,
+                        fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+            ),
+          ),
+        ),
+    );
+  }
+}
+
 
