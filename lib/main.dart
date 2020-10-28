740327eb162df1d0ef732b88ec25ea6762a86295
Derik <derik@oagro.com.br>
2020-10-28 15:59:39 -0300

    Adding second part of project

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+     backgroundColor: Colors.lightBlue,
+      body: Container()
+    )
+  ));
+}
+
