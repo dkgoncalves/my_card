416fec4b2d65ca73e0282fc1c4ca39586c3901ef
Derik <derik@oagro.com.br>
2020-10-29 16:01:36 -0300

    Finishing my_card project

--- lib/main.dart
+++ lib/main.dart
@@ -13,32 +13,75 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.teal,
             body: SafeArea(
             child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  CircleAvatar(
-                    backgroundImage: AssetImage('images/avatar2.jpg'),
-                    radius: 50.0,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage('images/avatar2.jpg'),
+                  radius: 50.0,
+                ),
+                Text(
+                  'Derik Gonçalves',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0
                   ),
-                  Text(
-                    'Derik Gonçalves',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Pacifico',
-                      fontSize: 40.0
+                ),
+                Text(
+                  'DEVELOPER',
+                  style: TextStyle(
+                      color: Colors.teal.shade100,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                  width:  150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal
                     ),
-                  ),
-                  Text(
-                    'DEVELOPER',
-                    style: TextStyle(
-                        color: Colors.teal.shade100,
+                    title: Text(
+                      '+55 53 98112-1314',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
                         fontFamily: 'SourceSansPro',
-                        fontSize: 20.0,
-                        letterSpacing: 2.5,
-                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0
+                      ),
                     ),
                   ),
-                ],
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal
+                    ),
+                    title: Text(
+                      'derik@derik.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0
+                      )
+                    )
+                  ),
+                )
+              ],
             ),
           ),
         ),
